f0c60bdd8ea755e8fc5141dfc62f6717cee979b0
root <root@firstvm.us-west4-b.c.devops-project-387202.internal>
2023-05-29 06:56:12 +0000

    monday

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main (String[] args) {
         System.out.println("hello world!!!");
-	System.out.println("welcome git !#@$%%");
+	System.out.println("welcome git from clone  !#@$%%");
 	System.out.println("hero hero");
     }
 }
